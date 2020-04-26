3aa3b0aad32c9a953973a6782bb878af59c76f4e
ilya-uzun <ilya.uzun@bk.ru>
2020-04-26 17:19:20 +0500

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-//Добавление локального хранилища
 var saveLine;
 
 function toLocal() {
@@ -9,42 +8,40 @@ function toLocal() {
 var list = document.querySelector('ul');
 list.addEventListener('click', function (ev){
   if(ev.target.tagName === "SPAN"){
-      ev.target.classList.toggle('checked');//Устаноавливаем класс 'checked'элементу списка
+      ev.target.classList.toggle('checked');
       toLocal();
-      }else if(ev.target.tagName === "SPAN2"){//
+      }else if(ev.target.tagName === "SPAN2"){
       var div = ev.target.parentNode;
       div.remove();
       toLocal();
   }
 }, false);
 
-// ввод по enter
 function checkKey(event){
   if(event.keyCode == "13"){
     newElement();
   }
 }
-// Добавление в список дел
+
 function newElement() {
-  var doc = document;//улучшение произодительности 
+  var doc = document;
   var li = doc.createElement('li');
-  var inputValue = doc.getElementById('newCase').value;// получаем значения поля ввода
+  var inputValue = doc.getElementById('newCase').value;
   var t = doc.createTextNode(inputValue);
-  li.appendChild(t);// сформирован элемент списка
+  li.appendChild(t);
 
   if(inputValue == ""){
     alert("Введите ваше дело!")
   }else{
-    doc.getElementById('list').appendChild(li);// добавляем в список дел
+    doc.getElementById('list').appendChild(li);
   }
-  // очистка значения списка
   doc.getElementById('newCase').value = "";
 
-  var done = doc.createElement('SPAN');//создать новый элемент
+  var done = doc.createElement('SPAN');
   done.className = "done";
   li.prepend(done);
 
-  var span = doc.createElement('SPAN2');//создать новый элемент
+  var span = doc.createElement('SPAN2');
   var txt = doc.createTextNode("Удалить");
   span.className = "close";
   span.append(txt);//
@@ -52,8 +49,6 @@ function newElement() {
   toLocal();
 }
 
-// вывод содержимого храниляща при обновлении вкладки
-// если есть значение ключа todos, то записываем в (localStorage) список это содержимое 
 if(localStorage.getItem('saveLine')){
   list.innerHTML = localStorage.getItem('saveLine');
 }
